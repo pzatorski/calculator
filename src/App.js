e15be63e2a202b52580969489a5308a9faa7ec33
Patryk Zatorski <patzat95@gmail.com>
2017-01-28 22:57:38 +0000

    Added buttons functionality

--- src/App.js
+++ src/App.js
@@ -3,7 +3,44 @@ import Display from './Display';
 import ButtonContainer from './ButtonContainer';
 
 class App extends Component {
+  state = {
+    age: 1,
+    display: ''
+  };
+
+  add = (numberOne, numberTwo) => {
+    return numberOne + numberTwo;
+  }
+
+  minus = (numberOne, numberTwo) => {
+    return numberOne - numberTwo;
+  }
+
+  multiple = (numberOne, numberTwo) => {
+    return numberOne * numberTwo;
+  }
+
+  divide = (numberOne, numberTwo) => {
+    return numberOne / numberTwo;
+  }
+
+  handleNumberPress = (value) => {
+    this.setState((prevState) => ({
+      display: `${prevState.display}${value}`
+    }))
+  }
+
+  handleOperationPress = (value) => {
+    if (value === 'AC') {
+      this.setState({
+        display: ''
+      })
+    }
+
+  }
+
   render() {
+
     return (
       <div
         style={{
@@ -14,9 +51,12 @@ class App extends Component {
           width: 410,
         }}
       >
-        <Display />
+        <Display displayValue={this.state.display}/>
 
-        <ButtonContainer />
+        <ButtonContainer
+          numberPress={this.handleNumberPress}
+          operationPress={this.handleOperationPress}
+        />
       </div>
     );
   }
--- src/ButtonContainer.js
+++ src/ButtonContainer.js
@@ -12,26 +12,26 @@ class ButtonContainer extends Component {
           flexWrap: 'wrap'
         }}
       >
-        <Button label="AC" />
-        <Button label="+/-" />
-        <Button label="%" />
-        <Button label="/" />
-        <Button label="7" />
-        <Button label="8" />
-        <Button label="9" />
-        <Button label="x" />
-        <Button label="4" />
-        <Button label="5" />
-        <Button label="6" />
-        <Button label="-" />
-        <Button label="1" />
-        <Button label="2" />
-        <Button label="3" />
-        <Button label="+" />
-        <Button label="0" />
+        <Button label="AC" operationPress={this.props.operationPress}/>
+        <Button label="+/-" operationPress={this.props.operationPress}/>
+        <Button label="%" operationPress={this.props.operationPress}/>
+        <Button label="/" operationPress={this.props.operationPress}/>
+        <Button label="7" numberPress={this.props.numberPress}/>
+        <Button label="8" numberPress={this.props.numberPress}/>
+        <Button label="9" numberPress={this.props.numberPress}/>
+        <Button label="x" operationPress={this.props.operationPress}/>
+        <Button label="4" numberPress={this.props.numberPress}/>
+        <Button label="5" numberPress={this.props.numberPress}/>
+        <Button label="6" numberPress={this.props.numberPress}/>
+        <Button label="-" operationPress={this.props.operationPress}/>
+        <Button label="1" numberPress={this.props.numberPress}/>
+        <Button label="2" numberPress={this.props.numberPress}/>
+        <Button label="3" numberPress={this.props.numberPress}/>
+        <Button label="+" operationPress={this.props.operationPress}/>
+        <Button label="0" numberPress={this.props.numberPress}/>
         <Button label="" />
-        <Button label="." />
-        <Button label="=" />
+        <Button label="." operationPress={this.props.operationPress}/>
+        <Button label="=" operationPress={this.props.operationPress}/>
       </div>
     )
   }
