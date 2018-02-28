3b2a6bc886d29bf25a07f23ac7f6dd66d730911b
Patryk Zatorski <patzat95@gmail.com>
2018-02-28 14:19:21 +0000

    add operation functionality

--- src/App.js
+++ src/App.js
@@ -4,42 +4,118 @@ import ButtonContainer from './ButtonContainer';
 
 class App extends Component {
   state = {
-    display: ''
+    display: '',
+    firstValue: '',
+    operatorPressed: ''
   };
 
   add = (numberOne, numberTwo) => {
     return numberOne + numberTwo;
-  }
+  };
 
   minus = (numberOne, numberTwo) => {
     return numberOne - numberTwo;
-  }
+  };
 
   multiple = (numberOne, numberTwo) => {
     return numberOne * numberTwo;
-  }
+  };
 
   divide = (numberOne, numberTwo) => {
     return numberOne / numberTwo;
-  }
+  };
 
-  handleNumberPress = (value) => {
-    this.setState((prevState) => ({
+  handleNumberPress = value => {
+    this.setState(prevState => ({
       display: `${prevState.display}${value}`
-    }))
-  }
+    }));
+  };
 
-  handleOperationPress = (value) => {
+  handleOperationPress = value => {
     if (value === 'AC') {
       this.setState({
         display: ''
-      })
+      });
     }
-
-  }
-
+    if (value === '+/-') {
+      if (this.state.firstValue < 0) {
+        this.setState({
+          firstValue: parseInt(this.state.display, 10),
+          operatorPressed: '',
+          display: this.state.firstValue
+        });
+      }
+      this.setState({
+        firstValue: parseInt(-this.state.display, 10),
+        operatorPressed: '+/-',
+        display: -this.state.display
+      });
+    }
+    if (value === '+') {
+      this.setState({
+        firstValue: parseInt(this.state.display, 10),
+        operatorPressed: '+',
+        display: ''
+      });
+    }
+    if (value === '-') {
+      this.setState({
+        firstValue: parseInt(this.state.display, 10),
+        operatorPressed: '-',
+        display: ''
+      });
+    }
+    if (value === 'x') {
+      this.setState({
+        firstValue: parseInt(this.state.display, 10),
+        operatorPressed: 'x',
+        display: ''
+      });
+    }
+    if (value === '/') {
+      this.setState({
+        firstValue: parseInt(this.state.display, 10),
+        operatorPressed: '/',
+        display: ''
+      });
+    }
+    if (value === '=') {
+      if (this.state.operatorPressed === '+') {
+        this.setState({
+          display: this.add(
+            this.state.firstValue,
+            parseInt(this.state.display, 10)
+          )
+        });
+      }
+      if (this.state.operatorPressed === '-') {
+        this.setState({
+          display: this.minus(
+            this.state.firstValue,
+            parseInt(this.state.display, 10)
+          )
+        });
+      }
+      if (this.state.operatorPressed === 'x') {
+        this.setState({
+          display: this.multiple(
+            this.state.firstValue,
+            parseInt(this.state.display, 10)
+          )
+        });
+      }
+      if (this.state.operatorPressed === '/') {
+        this.setState({
+          display: this.divide(
+            this.state.firstValue,
+            parseInt(this.state.display, 10)
+          )
+        });
+      }
+    }
+  };
   render() {
-
+    console.log(this.state);
     return (
       <div
         style={{
@@ -47,10 +123,10 @@ class App extends Component {
           flexDirection: 'column',
           border: '1px solid black',
           height: 617,
-          width: 410,
+          width: 410
         }}
       >
-        <Display displayValue={this.state.display}/>
+        <Display displayValue={this.state.display} />
 
         <ButtonContainer
           numberPress={this.handleNumberPress}
